ad177cd7a95622c58214fe6f585d9d70e2bbf49e
cisuuuuuu <patryk23092@gmail.com>
2025-02-19 14:59:46 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function przewin(sectionId) {
-    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' })
 }
 
 
@@ -13,14 +13,11 @@ function wizyta()
         document.getElementById("umowiona_wizyta").innerHTML="Dziękujemy, Twoja wizyta została zaplanowana na: "+ data
     }
 }
-    // document.getElementById("dane_osobowe").value
-    // document.getElementById("numer_telefonu").value
-    // document.getElementById("rodzaj").value
-    // document.getElementById("usterka").value
+    
 function pokaz() {
     document.getElementById("zgloszenie").style.display="none"
     document.getElementById("zakonczenie").style.display="none"
-    document.getElementById("imienazwisko").style.display="block"
+    document.getElementById("formularz").style.display="block"
 }
 
 function dalej() {
@@ -30,16 +27,21 @@ function dalej() {
     var RodzajZgloszenia = document.getElementById("rodzaj").value
     var Usterka = document.getElementById("usterka").value
     if ( DaneOsobowe == "" || Email == "" || NrTelefonu == "" || RodzajZgloszenia == "" || Usterka == "") {
-        alert("Proszę wypełnić wszystkie pola formularza.");
+        alert("Proszę wypełnić wszystkie pola formularza.")
     }
     else {
-        document.getElementById("imienazwisko").style.display="none"
+        document.getElementById("formularz").style.display="none"
         document.getElementById("potwierdzenie").style.display="block"
-        document.getElementById("wynik").innerHTML=DaneOsobowe+" "+Email+" "+NrTelefonu+" "+RodzajZgloszenia+" "+Usterka
+        document.getElementById("wynik").innerHTML="<p class='dane'><strong>Imie i Nazwisko:</strong> "+DaneOsobowe+"</p><p class='dane'><strong>Adres email:</strong> "+Email+"</p><p class='dane'><strong>Numer telefonu:</strong> "+NrTelefonu+"</p><p class='dane'><strong>Rodzaj zgłoszenia:</strong> "+RodzajZgloszenia+"</p><p class='dane'><strong>Opis usterki:</strong> "+Usterka
     }
 
 }
 function zatwierdz() {
     document.getElementById("potwierdzenie").style.display="none"
     document.getElementById("zakonczenie").style.display="block"
+}
+
+function cofnij() {
+    document.getElementById("potwierdzenie").style.display="none"
+    document.getElementById("formularz").style.display="block"
 }
